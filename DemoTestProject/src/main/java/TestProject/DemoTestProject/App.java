cefd29a9b94d5b51220991f34431ea21a3d6df68
Naveen <naveenprasadsomasundaram@gmail.com>
2020-10-02 19:53:04 +0530

    initial commit for maven jenkins test

---
+++ DemoTestProject/src/main/java/TestProject/DemoTestProject/App.java
@@ -0,0 +1,13 @@
+package TestProject.DemoTestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! this is a maven test project for Jenkins.." );
+    }
+}
